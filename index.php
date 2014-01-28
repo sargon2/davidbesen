3d06fd8de7a89b8ef1fddedee1945a00a3da5427
David Besen <dbesen@gmail.com>
2014-01-27 19:06:51 -0700

    Remove obsolete links

--- index.php
+++ index.php
@@ -5,9 +5,6 @@
 <body>
 David K. Besen<br />
 My <a href="http://davidbesen.com/shoebox">photography</a><br />
-<? //My resume in <a href="http://davidbesen.com/resume.doc">doc</a> or <a href="http://davidbesen.com/resume.pdf">pdf</a><br /> ?>
 My <a href="http://davidbesen.com/graphics_project/">graphics project</a> from <a href="http://www.cs.colostate.edu/~cs410/">CS410</a> FA07<br />
-My <a href="http://davidbesen.com/heart/">heart</a><br />
-<? //My <a href="http://davidbesen.com/gps/">current location</a>?<br /> ?>
 </body>
 </html>
